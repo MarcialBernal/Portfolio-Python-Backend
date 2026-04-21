16a7864a7b189e8dcc9a46cf79cbb6acba2700e1
Marcial Bernal <marcialb9328@gmail.com>
2026-04-20 21:25:29 -0700

    conectado router de companion al main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from services.gym_assistant.routers.routes import router as gym_router
 from services.web_scraper.routers.routes import router as webscraper_router
 from services.api_consumption.routers.routes import router as apiconsumption_router
 from services.auto_ml.routers.routes import router as auto_ml_router
+from services.companion.routers.companion_routes import router as companion_router
 
 app = FastAPI()
 
@@ -15,6 +16,7 @@ app.include_router(gym_router, prefix="/gym")
 app.include_router(webscraper_router, prefix="/webscraper")
 app.include_router(apiconsumption_router, prefix="/apiconsumption")
 app.include_router(auto_ml_router, prefix="/auto-ml")
+app.include_router(companion_router, prefix="/companion")
 
 # -----------------------------
 #   ROOT
